bd338a096add6aefae4249ee5e44b5bc7461bb08
hanss2006 <hanss@live.ru>
2021-05-16 23:35:18 +0300

    Добавлены веб методы по всем таблицам

--- src/main/java/com/ettiv/docs/domain/TypeDocRepository.java
+++ src/main/java/com/ettiv/docs/domain/TypeDocRepository.java
@@ -3,7 +3,9 @@ import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "types", path = "types")
 public interface TypeDocRepository extends PagingAndSortingRepository<TypeDoc, Integer> {
 
     List<TypeDoc> findByName(@Param("name") String name);
--- src/main/java/com/ettiv/docs/domain/VidDocRepository.java
+++ src/main/java/com/ettiv/docs/domain/VidDocRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "vid", path = "vid")
+@RepositoryRestResource(collectionResourceRel = "vids", path = "vids")
 public interface VidDocRepository extends PagingAndSortingRepository<VidDoc, Integer> {
 
     List<VidDoc> findByName(@Param("name") String name);
