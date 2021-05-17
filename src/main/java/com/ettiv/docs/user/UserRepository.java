27ad1cf27c18d18da6abc5df8f1bd074aadd8482
hanss2006 <hanss@live.ru>
2021-05-17 23:29:06 +0300

    Добавлены веб методы по всем таблицам

--- src/main/java/com/ettiv/docs/user/UserRepository.java
+++ src/main/java/com/ettiv/docs/user/UserRepository.java
@@ -3,27 +3,26 @@ import java.util.List;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-@Secured("ROLE_USER")
+
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     List<User> findByEmail(@Param("email") String email);
     List<User> findByLastName(@Param("lastName") String lastName);
 
-    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @Override
     void delete(User user);
 
-    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @Override
     void deleteAll(Iterable<? extends User> users);
 
-    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @Override
     void deleteAll();
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @Override
     User save(User s);
 }
