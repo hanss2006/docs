d483bef2b6e274c90f8a653289f547909dbf93f7
hanss2006 <hanss@live.ru>
2021-05-22 19:10:06 +0300

    Добавлены @CrossOrigin("*")

--- src/main/java/com/ettiv/docs/domain/DocRepository.java
+++ src/main/java/com/ettiv/docs/domain/DocRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("*")
+@CrossOrigin
 public interface DocRepository extends PagingAndSortingRepository<Doc, Long> {
 
     List<Doc> findByName(@Param("name") String name);
--- src/main/java/com/ettiv/docs/domain/VidDocRepository.java
+++ src/main/java/com/ettiv/docs/domain/VidDocRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("*")
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "vids", path = "vids")
 public interface VidDocRepository extends PagingAndSortingRepository<VidDoc, Long> {
 
